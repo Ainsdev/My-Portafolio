5a761fc5cd190207ce5b753e450eac817ea1e4f4
Ainsdev <ainsn1654@gmail.com>
2022-10-20 00:09:23 -0300

    Animation of the first view

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,41 +16,35 @@ const Home: NextPage = () => {
 
   return (
     <HomeLayout >
-      <main className='w-screen h-[1920px]'>
-        <section className='w-screen'>
-          <div className='absolute top-40 flex w-screen overflow-hidden sm:hidden'>
-            <motion.h1
-              initial="hidden"
-              animate={{ x: scroll * -6, opacity: 1 }}
-              className='text-9xl md:text-[250px]  font-bold whitespace-nowrap '>I LOVE BUILD THINGS</motion.h1>
-          </div>
-          <div className='absolute top-52 flex w-screen overflow-hidden'>
-            <motion.h1
-              initial="hidden"
-              animate={{ x: scroll * 2, opacity: 1 }}
-              className='text-9xl md:text-[250px]  font-bold whitespace-nowrap text-primary'>I LOVE BUILD</motion.h1>
-          </div>
-            <WindowTab />
-          <div className='px-20 relative z-10'>
-            <motion.h1
-              initial="hidden"
-              animate={{ x: scroll * -2, opacity: 1 }}
-              className='text-[250px] font-bold whitespace-nowrap tracking-wide absolute top-[528px] text-accent-2 z-50'>THINGS</motion.h1>
-            </div>
-        </section>
-      </main>
-      <main className='w-screen h-screen'>
-        <div className="w-1/2 transition-colors ease-linear shadow-md absolute ">
-          <div className="w-full h-12 rounded-t-lg  bg-base-2 flex justify-end items-center space-x-1.5 px-4">
-            <span className="w-3 h-3 border-2 border-transparent dark:border-green-400 rounded-full bg-green-400 dark:bg-transparent"></span>
-            <span className="w-3 h-3 border-2 border-transparent dark:border-yellow-400 rounded-full bg-yellow-400 dark:bg-transparent"></span>
-            <span className="w-3 h-3 border-2 border-transparent dark:border-red-400 rounded-full bg-red-400 dark:bg-transparent "></span>
-          </div>
-          <div className="bg-gray-100 dark:bg-gray-700 border-t-0 w-full h-96 rounded-b-lg"></div>
-        </div>
-      </main>
+      {FirstHome(scroll)}
     </HomeLayout>
   )
 }
 
 export default Home
+function FirstHome(scroll: number) {
+  return <main className='w-screen h-[1920px]'>
+    <section className='w-screen'>
+      <div className='absolute top-40 flex w-screen overflow-hidden md:hidden'>
+        <motion.h1
+          initial="hidden"
+          animate={{ x: scroll * -6, opacity: 1 }}
+          className='text-9xl md:text-[250px]  font-bold whitespace-nowrap '>I LOVE BUILD THINGS</motion.h1>
+      </div>
+      <div className='absolute top-48 flex w-screen overflow-hidden'>
+        <motion.h1
+          initial="hidden"
+          animate={{ x: scroll * 2, opacity: 1 }}
+          className='text-9xl md:text-[250px]  font-bold whitespace-nowrap text-primary hidden md:flex'>I LOVE TO BUILD</motion.h1>
+      </div>
+      <WindowTab />
+      <div className='px-20 relative z-10'>
+        <motion.h1
+          initial="hidden"
+          animate={{ x: scroll * -2, opacity: 1 }}
+          className='hidden sm:flex text-[250px] font-bold whitespace-nowrap tracking-wide absolute top-[528px] text-accent-2 z-50 '>THINGS</motion.h1>
+      </div>
+    </section>
+  </main>
+}
+
