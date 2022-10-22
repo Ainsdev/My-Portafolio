7007d08c21e540fbda1423b6369c3fd5a82751ce
Ainsdev <ainsn1654@gmail.com>
2022-10-22 12:27:01 -0300

    animation features

--- layout/components/WindowTab.tsx
+++ layout/components/WindowTab.tsx
@@ -13,9 +13,14 @@ const WindowTab = () => {
     return (
         <motion.div
         animate={{ x: scroll * 0.2, opacity: 1,rotateZ: scroll * 0.008 }}
-        className="sm:w-[60%] absolute top-1/3 sm:right-16 z-10
-        w-[90%] right-0">
-            <div className="opacity-95 w-full h-10 rounded-t-lg  hover:opacity-100 bg-gray-800 flex justify-end items-center space-x-1.5 px-4">
+            transition={{
+                x: { type: "spring", stiffness: 100 },
+                duration: 0.8,
+                delay: 0.01,
+            }}
+        className="sm:w-[60%] absolute top-1/3 sm:right-16 z-30
+        w-[90%] right-0 cursor-default">
+            <div className="opacity-95 w-full h-10 rounded-t-lg  bg-gray-800 flex justify-end items-center space-x-1.5 px-4">
                 <span className="w-3 h-3 border-2 border-transparent dark:border-green-400 rounded-full bg-green-400 dark:bg-transparent"></span>
                 <span className="w-3 h-3 border-2 border-transparent dark:border-yellow-400 rounded-full bg-yellow-400 dark:bg-transparent"></span>
                 <span className="w-3 h-3 border-2 border-transparent dark:border-red-400 rounded-full bg-red-400 dark:bg-transparent "></span>
@@ -24,7 +29,9 @@ const WindowTab = () => {
                 <div>
                     <h1 className='sm:text-5xl font-bold text-center sm:pb-5 text-2xl'>Welcome to my Portfolio✌️</h1>
                 </div>
-                <HomeButton title='Minimalist' link='/simple/home' description='More Simple Portafolio' color='bg-accent' />
+                <div className='z-50'>
+                    <HomeButton title='Minimalist' link='/simple/home' description='More Simple Portafolio' color='bg-accent' />
+                </div>
                 <HomeButton title='Contact Me' link='/' description='I Answer quickly :)' color='bg-white' />
                 <div className='absolute bottom-0 right-0 w-28 h-8 bg-blue-300 rounded-tl-xl flex justify-center items-center gap-3 '>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,44 +7,105 @@ import HomeLayout from '../layout/layouts/HomeLayout'
 const Home: NextPage = () => {
   const { scrollY } = useScroll();
   const [scroll, setscroll] = useState<number>(0);
+  useEffect(() => {
+      scrollY.onChange((latest) => {
+       if (latest < 950) {
+        setscroll(latest)
+      }
+    });
+  }, [scrollY])
+  // custom cursor
+  const [cursor, setCursor] = useState({
+    x: 0,
+    y: 0,
+  })
+  const [cursorProps, setCursorProps] = useState('default')
 
   useEffect(() => {
-    return scrollY.onChange((latest) => {
-      setscroll(latest)
-    })
-  }, [scrollY])
-
+    const mouseMove = (e: MouseEvent) => {
+      setCursor({
+        x: e.clientX,
+        y: e.clientY,
+      })
+    }
+    window.addEventListener('mousemove', mouseMove)
+    return () => {
+      window.removeEventListener('mousemove', mouseMove)
+    }
+  }, [])
+  const variants = {
+    default: {
+      x: cursor.x -24,
+      y: cursor.y -24,
+      zIndex: 999,
+      backgroundColor: '#fff',
+    },
+    hovered:  {
+      x: cursor.x -24,
+      y: cursor.y -24,
+      zIndex: 30,
+    },
+    text: {
+      backgroundColor: '#fff',
+      x: cursor.x -24,
+      y: cursor.y -24,
+      zIndex: 99,
+      width: 120,
+      height: 120,
+      mixBlendMode: 'difference',
+    }
+  }
   return (
     <HomeLayout >
-      {FirstHome(scroll)}
+      <main className='w-screen h-[1920px] flex flex-col scroll-smooth cursor-none'>
+        <motion.div
+        variants={variants}
+        animate= {cursorProps}
+          className='w-12 h-12 rounded-full top-0 left-0 fixed pointer-events-none invisible sm:visible'></motion.div>
+        <section className='w-screen'>
+          <div className='absolute top-40 flex w-screen overflow-hidden md:hidden'>
+            <motion.h1
+              initial="hidden"
+              animate={{ x: scroll * -6, opacity: 1 }}
+              className='text-9xl md:text-[220px]  font-bold whitespace-nowrap '>I LOVE BUILD THINGS</motion.h1>
+          </div>
+          <div className='absolute top-48 flex w-screen overflow-hidden' onMouseEnter={() => setCursorProps("text")}
+            onMouseLeave={() => setCursorProps('default')}>
+            <motion.h1
+              initial="hidden"
+              animate={{ x: scroll * 1.6, opacity: 1 }}
+              transition={{
+                x: { type: "spring", stiffness: 100 },
+                duration: 0.8,
+                delay: 0.01,
+              }}
+              className='text-9xl md:text-[220px]  font-bold whitespace-nowrap text-primary hidden md:flex'>I LOVE BUILD</motion.h1>
+          </div>
+          <div onMouseEnter={() => setCursorProps("hovered")} onMouseLeave={()=>setCursorProps('default')} >
+            <WindowTab />
+          </div>
+          <div className='px-20 relative z-40' onMouseEnter={() => setCursorProps("text")}
+              onMouseLeave={() => setCursorProps('default')}>
+            <motion.h1
+              initial="hidden"
+              animate={{ x: scroll * -1.6, opacity: 1 }}
+              transition={{
+                x: { type: "spring", stiffness: 100 },
+                duration: 0.8,
+                delay: 0.01,
+              }}
+              className='hidden sm:flex text-[220px] font-bold whitespace-nowrap tracking-wide absolute top-[528px] text-accent-2'>THINGS</motion.h1>
+          </div>
+        </section>
+        <div className='absolute bottom-5 left-1/2 animate-bounce'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          </svg>
+        </div>
+      </main>
     </HomeLayout>
   )
 }
 
 export default Home
-function FirstHome(scroll: number) {
-  return <main className='w-screen h-[1920px]'>
-    <section className='w-screen'>
-      <div className='absolute top-40 flex w-screen overflow-hidden md:hidden'>
-        <motion.h1
-          initial="hidden"
-          animate={{ x: scroll * -6, opacity: 1 }}
-          className='text-9xl md:text-[250px]  font-bold whitespace-nowrap '>I LOVE BUILD THINGS</motion.h1>
-      </div>
-      <div className='absolute top-48 flex w-screen overflow-hidden'>
-        <motion.h1
-          initial="hidden"
-          animate={{ x: scroll * 2, opacity: 1 }}
-          className='text-9xl md:text-[250px]  font-bold whitespace-nowrap text-primary hidden md:flex'>I LOVE TO BUILD</motion.h1>
-      </div>
-      <WindowTab />
-      <div className='px-20 relative z-10'>
-        <motion.h1
-          initial="hidden"
-          animate={{ x: scroll * -2, opacity: 1 }}
-          className='hidden sm:flex text-[250px] font-bold whitespace-nowrap tracking-wide absolute top-[528px] text-accent-2 z-50 '>THINGS</motion.h1>
-      </div>
-    </section>
-  </main>
-}
 
