9833027b1e9da7be362d91696558617650fd843d
Ainsdev <ainsn1654@gmail.com>
2022-10-25 12:30:30 -0300

    typescript problem checked

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Home: NextPage = () => {
   const { scrollY } = useScroll();
   const [scroll, setscroll] = useState<number>(0);
   useEffect(() => {
-      scrollY.onChange((latest) => {
-       if (latest < 950) {
+    scrollY.onChange((latest) => {
+      if (latest < 950) {
         setscroll(latest)
       }
     });
@@ -35,20 +35,20 @@ const Home: NextPage = () => {
   }, [])
   const variants = {
     default: {
-      x: cursor.x -24,
-      y: cursor.y -24,
+      x: cursor.x - 24,
+      y: cursor.y - 24,
       zIndex: 999,
       backgroundColor: '#fff',
     },
-    hovered:  {
-      x: cursor.x -24,
-      y: cursor.y -24,
+    hovered: {
+      x: cursor.x - 24,
+      y: cursor.y - 24,
       zIndex: 30,
     },
     text: {
       backgroundColor: '#fff',
-      x: cursor.x -24,
-      y: cursor.y -24,
+      x: cursor.x - 24,
+      y: cursor.y - 24,
       zIndex: 99,
       width: 120,
       height: 120,
@@ -59,8 +59,8 @@ const Home: NextPage = () => {
     <HomeLayout >
       <main className='w-screen h-[1920px] flex flex-col scroll-smooth cursor-none'>
         <motion.div
-        variants={variants}
-        animate= {cursorProps}
+          variants={variants as any}
+          animate={cursorProps}
           className='w-12 h-12 rounded-full top-0 left-0 fixed pointer-events-none invisible sm:visible'></motion.div>
         <section className='w-screen'>
           <div className='absolute top-40 flex w-screen overflow-hidden md:hidden'>
@@ -76,25 +76,23 @@ const Home: NextPage = () => {
               animate={{ x: scroll * 1.6, opacity: 1 }}
               transition={{
                 x: { type: "spring", stiffness: 100 },
-                duration: 0.8,
                 delay: 0.01,
               }}
               className='text-9xl md:text-[220px]  font-bold whitespace-nowrap text-primary hidden md:flex'>I LOVE BUILD</motion.h1>
           </div>
-          <div onMouseEnter={() => setCursorProps("hovered")} onMouseLeave={()=>setCursorProps('default')} >
+          <div onMouseEnter={() => setCursorProps("hovered")} onMouseLeave={() => setCursorProps('default')} >
             <WindowTab />
           </div>
           <div className='px-20 relative z-40' onMouseEnter={() => setCursorProps("text")}
-              onMouseLeave={() => setCursorProps('default')}>
+            onMouseLeave={() => setCursorProps('default')}>
             <motion.h1
               initial="hidden"
               animate={{ x: scroll * -1.6, opacity: 1 }}
               transition={{
                 x: { type: "spring", stiffness: 100 },
-                duration: 0.8,
                 delay: 0.01,
               }}
-              className='hidden sm:flex text-[220px] font-bold whitespace-nowrap tracking-wide absolute top-[528px] text-accent-2'>THINGS</motion.h1>
+              className='hidden md:flex text-[220px] font-bold whitespace-nowrap tracking-wide absolute top-[528px] text-accent-2'>THINGS</motion.h1>
           </div>
         </section>
         <div className='absolute bottom-5 left-1/2 animate-bounce'>
@@ -102,6 +100,9 @@ const Home: NextPage = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
           </svg>
         </div>
+        <section>
+          <div></div>
+        </section>
       </main>
     </HomeLayout>
   )
