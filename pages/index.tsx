e3ae2cc45a10dc368bf2695872fadeb04b04284f
Ainsdev <ainsn1654@gmail.com>
2022-11-11 18:36:11 -0300

    finish the UI of about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,6 +53,15 @@ const Home: NextPage = () => {
       width: 120,
       height: 120,
       mixBlendMode: 'difference',
+    },
+    sText: {
+      backgroundColor: '#fff',
+      x: cursor.x - 24,
+      y: cursor.y - 24,
+      zIndex: 99,
+      width: 120,
+      height: 120,
+      mixBlendMode: 'overlay',
     }
   }
   return (
@@ -100,12 +109,55 @@ const Home: NextPage = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
           </svg>
         </div>
-        <section>
-          <div></div>
-        </section>
+        <div className='h-screen w-screen'></div>
+              <div className='px-20 inline-block z-40 mt-48 w-max'>
+                <h1 className='text-7xl font-bold text-accent'>About Me</h1>
+              </div>  
+        {AboutMe()}
       </main>
     </HomeLayout>
   )
+
+  function AboutMe() {
+    return <section className='w-screen h-96 flex justify-evenly mt-20'>
+      <article className='w-full h-full px-20' onMouseEnter={() => setCursorProps("sText")}
+        onMouseLeave={() => setCursorProps('default')}>
+        <h3 className='text-4xl font-bold flex align-middle py-5 justify-start items-center'>I am <h1 className='px-2 text-6xl text-primary font-bold'>Guilad</h1>from<p className='px-2'>Chile</p></h3>
+        <p>I love solving problems with technology, thats why
+          Im an enthusiast of impact the user with
+          creative & innovate solutions.</p>
+      </article>
+      <article className='w-full h-full  relative overflow-hidden' style={{
+        clipPath: 'polygon(0 2%, 100% 0, 100% 100%, 12% 100%)'
+      }}>
+        <div className='text-9xl flex left-0 animate-marquee-infinite'>
+          <h1 className='font-bold pl-48'>Creative</h1>
+          <h1 className='font-bold pl-20'>Innovative</h1>
+          <h1 className='font-bold pl-52'>Commited</h1>
+          <h1 className='font-bold pl-48'>Hunger</h1>
+          <h1 className='font-bold pl-20'>Innovative</h1>
+          <h1 className='font-bold pl-52'>Commited</h1>
+          <h1 className='font-bold pl-48'>Creative</h1>
+          <h1 className='font-bold pl-20'>Innovative</h1>
+          <h1 className='font-bold pl-52'>Commited</h1>
+        </div>
+        <div className='text-8xl flex left-0 animate-marquee-infinite mt-10'>
+          <h1 className='font-bold pl-20'>Enthusiastic</h1>
+          <h1 className='font-bold pl-10'>Commited</h1>
+          <h1 className='font-bold pl-20'>Passionate</h1>
+          <h1 className='font-bold pl-20'>Optimistic</h1>
+          <h1 className='font-bold pl-10'>VIEW</h1>
+          <h1 className='font-bold pl-20'>Passionate</h1>
+          <h1 className='font-bold pl-20'>Enthusiastic</h1>
+          <h1 className='font-bold pl-10'>Innovative</h1>
+          <h1 className='font-bold pl-20'>Passionate</h1>
+          <h1 className='font-bold pl-20'>Enthusiastic</h1>
+          <h1 className='font-bold pl-10'>Innovative</h1>
+          <h1 className='font-bold pl-20'>Passionate</h1>
+        </div>
+      </article>
+    </section>
+  }
 }
 
 export default Home
